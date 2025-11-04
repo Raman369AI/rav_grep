3fef43f1efd06b054e8e3b2af7ace952b418576c
Raman369AI <ramanraj33@gmail.com>
2025-11-03 20:48:04 -0600

    Modular GREP

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,60 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
+use rav_grep::search;
 fn main() {
 let args: Vec<String> = env::args().collect();
-let query = &args[1];
-let filename = &args[2];
-println!("Searching for {}", query);
-println!("In file {}", filename);
-let contents = fs::read_to_string(filename)
-        .expect("Should have been able to read the file");
+let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+        
+    });
+        println!("Searching for {}", config.query);
+        println!("In file {}", config.file_path);
+        //Pattern matching to handle the Result returned by the run function.
+        if let Err(e) = run(config) {
+            println!("Application error: {e}");
+            process::exit(1);
+        }
+        // run(config);
 
-    println!("With text:\n{contents}");
 }
+
+fn run(config: Config)-> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    //Expect here fails the code for us if we cannot read the file or any other error occurs.
+        for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str>  {
+        if args.len() < 3 {
+
+            //Panic stops the execution of the program immediately and displays the message passed to it.
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+//An expression returns the value, now the difference between statement and expression is that expression has no semicolon at the end while statement has a semicolon at the end.
+// It also return a Config struct with the query and file_path fields set to the values passed as command line arguments.
+//Statements return None while expressions return a value.
+
+
 
 
 //This is a Rust program to replicate the Grep prograp.
